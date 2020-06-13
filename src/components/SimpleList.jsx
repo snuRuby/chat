e9d0b47a24092765ceda68489ba25cdd8016f4e8
DongHoon <jsmun5@naver.com>
2020-06-14 00:10:57 +0900

    fix SimpleList

--- src/components/SimpleList.jsx
+++ src/components/SimpleList.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import { Box, List, ListItem, ListItemText } from '@material-ui/core';
+import {
+  Box, List, ListItem, ListItemText,
+} from '@material-ui/core';
+import PropTypes from 'prop-types';
 
-const SimpleList = ({ items }) => {
-  return (
-    <Box width={500}>
-      <List>
-        {items.map((item, idx) => (
-          // add onClick
-          <ListItem button divider key={idx}>
-            <ListItemText primary={item} />
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
+const SimpleList = ({ items }) => (
+  <Box width={500}>
+    <List>
+      {items.map((item, idx) => (
+        // add onClick
+        <ListItem button divider key={+idx}>
+          <ListItemText primary={item} />
+        </ListItem>
+      ))}
+    </List>
+  </Box>
+);
+
+SimpleList.propTypes = {
+  items: PropTypes.string.isRequired,
 };
 
 export default SimpleList;
