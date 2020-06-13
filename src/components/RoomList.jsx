be8293d61e739c694d2fe36286bf9b3f8c6b68a7
DongHoon <jsmun5@naver.com>
2020-06-13 15:21:33 +0900

    fix RoomList

--- src/components/RoomList.jsx
+++ src/components/RoomList.jsx
@@ -4,11 +4,13 @@ import {
 import React, {
   useCallback, useEffect, useMemo, useState,
 } from 'react';
+import PropTypes from 'prop-types';
 
 import Api from '../api';
 import SimpleList from './SimpleList';
+import InputForm from './InputForm';
 
-const RoomList = () => {
+const RoomList = ({ auth }) => {
   const api = useMemo(() => new Api(), []);
   const [rooms, setRooms] = useState();
   const loadRooms = useCallback(async () => {
@@ -17,10 +19,20 @@ const RoomList = () => {
   }, [api]);
   useEffect(() => {
     loadRooms();
-    setInterval(() => {
-      loadRooms();
-    }, 3000);
   }, [api, loadRooms]);
+
+  const CreateRoom = () => {
+    const createRoom = async (name) => {
+      await api.createRoom(name);
+      await loadRooms();
+    };
+    return (
+      <Box>
+        <Typography variant="h5">Create Room</Typography>
+        <InputForm label="Room Name" onSubmit={createRoom} />
+      </Box>
+    );
+  };
 
   if (!rooms) {
     return <CircularProgress />;
@@ -29,9 +41,14 @@ const RoomList = () => {
 
   return (
     <Box>
+      {auth ? <CreateRoom /> : null}
       <Typography variant="h5">Room List</Typography>
       <SimpleList items={items} />
     </Box>
   );
 };
 export default RoomList;
+
+RoomList.propTypes = {
+  auth: PropTypes.bool.isRequired,
+};
