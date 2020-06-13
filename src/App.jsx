502d5c98acc2b24551dc64c0d99f237b6405ad1e
Donghoon <donghoon@beautyops.co.kr>
2020-06-13 12:08:26 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Button, CircularProgress, Link, Typography } from '@material-ui/core';
+import {
+  Box, Button, CircularProgress, Typography,
+} from '@material-ui/core';
+import React, {
+  useCallback, useEffect, useMemo, useState,
+} from 'react';
+import { Link } from 'react-router-dom';
 
 import Api from './api';
 import { SimpleList, InputForm } from './components';
 
 const App = () => {
-  const api = new Api();
+  const api = useMemo(() => new Api(), []);
   const [rooms, setRooms] = useState();
   const [user, setUser] = useState();
-  const loadRooms = async () => {
+  const loadRooms = useCallback(async () => {
     const response = await api.getRooms();
     setRooms(response);
-  }
+  }, [api]);
   useEffect(() => {
     loadRooms();
-    api.login().then(({ name, success }) => success ? setUser(name) : null);
+    api.login().then(({ name, success }) => (success ? setUser(name) : null));
     setInterval(() => {
       loadRooms();
     }, 3000);
-  }, []);
+  }, [api, loadRooms]);
 
   if (!rooms) {
     return <CircularProgress />;
@@ -27,18 +32,18 @@ const App = () => {
   const createRoom = async (name) => {
     await api.createRoom(name);
     loadRooms();
-  }
+  };
 
   return (
     <Box>
       {user
-      ? <Button color="primary" variant="contained">Create Room</Button>
-      : <Link component="button">Login</Link>}
+        ? <Button color="primary" variant="contained">Create Room</Button>
+        : <Button component={Link} to="/">Login</Button>}
       <Typography variant="h5">Create Room</Typography>
-      <InputForm label="Room Name" onSubmit={createRoom}/>
+      <InputForm label="Room Name" onSubmit={createRoom} />
       <Typography variant="h5">Room List</Typography>
       <SimpleList items={items} />
     </Box>
   );
-}
+};
 export default App;
